67f56944341b67289c323a60074999a1732cf261
supriti pal <supritipal2016@gmail.com>
2024-12-16 17:40:44 +0530

    toast,loader,localStorage

---
+++ lib/components/loading_spinner.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hrms/styleColor.dart';
+
+class LoadingSpinner extends StatelessWidget {
+  const LoadingSpinner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const CircularProgressIndicator(
+        color: AppColors.greyShade,
+      ),
+    );
+  }
+}
