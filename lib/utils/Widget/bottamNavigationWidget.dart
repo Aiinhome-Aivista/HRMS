fd33b6b45bb9eae5dd6b4ceb0a01e259fdba8f12
Manoj <mmanojpal777@gmail.com>
2024-12-16 18:28:24 +0530

    login api

--- lib/utils/Widget/bottamNavigationWidget.dart
+++ lib/utils/Widget/bottamNavigationWidget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hrms/styleColor.dart';
 import 'package:hrms/utils/Screens/documentArchiveScreen.dart';
 import 'package:hrms/utils/Screens/leaveScreen.dart';
+import 'package:hrms/utils/Screens/loginSreen.dart';
 
 class BottamnavigationBar extends StatefulWidget {
   const BottamnavigationBar({Key? key}) : super(key: key);
@@ -50,7 +51,10 @@ class _BottamnavigationBarState extends State<BottamnavigationBar> {
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
                 // Implement logout logic here
               },
               child: const Text('Logout'),
